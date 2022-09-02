9663961d32bfe7fd41f7bb5361b7df738c5c6b7f
yuna gando <gando@grean.tech>
2022-09-02 16:03:50 +0900

    no5まで完了

--- script.js
+++ script.js
@@ -1,25 +1,40 @@
-const list = document.querySelector('.output ul');
-const totalBox = document.querySelector('.output p');
+// const list = document.querySelector('.output ul');
+// const totalBox = document.querySelector('.output p');
 let total = 0;
-list.innerHTML = '';
-totalBox.textContent = '';
+// list.innerHTML = '';
+// totalBox.textContent = '';
 
 // No.1
-let products = ['パンツ:6.99','靴下:5.99','T シャツ:14.99', 'ズボン:31.99', '靴:23.99'];
+let products = ['パンツ:6.99', '靴下:5.99', 'T シャツ:14.99', 'ズボン:31.99', '靴:23.99'];
 
-for (let i = 0; i < products.length ; i++) { // No.2
+// console.log(products[0]);
 
-// No.3
-let array = products.split(':');
-let name = array[0];
-let price =array[1];
+for (let i = 0; i < products.length; i++) { // No.2
 
-// No.4
+  // No.3
+  // itemsには1巡目は'パンツ:6.99'、2巡目は'靴下:5.99'、、、が入る
+  // let items = ['パンツ','6.99'];
+  // let items = ['靴下','5.99'];
+  let items = products[i].split(':');
 
-// No.5
+  let itemName = items[0];
+  let itemPrice = items[1];
 
+  // No.4
+  // 文字列を数値に変換
+  let numberPrice = Number.parseFloat(itemPrice);
 
-  const listItem = document.createElement('li');
-  listItem.textContent = itemText;
-  list.appendChild(listItem);
+  // No.5
+  let itemText = 0;
+  total += numberPrice;
+
+  // Mo.6
+  itemText =itemName + ' — $' +itemPrice;
+
+  // const listItem = document.createElement('li');
+  // listItem.textContent = itemText;
+  // list.appendChild(listItem);
+  console.log(itemText);
 }
+
+console.log(27.97 + 31.99);
